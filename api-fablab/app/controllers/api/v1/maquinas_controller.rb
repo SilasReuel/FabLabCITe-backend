35bf5177c0afaf00f2c5b3ccd3f1ab1791022f3f
SilasReuel <silas.reuel@unesp.br>
2025-01-28 11:20:18 -0300

    feat(api): adiciona tabelas 'usuarios' e 'maquinas' com controllers e models
    Cria as tabelas 'usuarios' e 'maquinas' no banco de dados. Adiciona os controllers e models correspondentes para gerenciar as operações relacionadas a essas tabelas.

---
+++ api-fablab/app/controllers/api/v1/maquinas_controller.rb
@@ -0,0 +1,51 @@
+class Api::V1::MaquinasController < ApplicationController
+  before_action :set_maquina, only: %i[ show update destroy ]
+
+  # GET /maquinas
+  def index
+    @maquinas = Maquina.all
+
+    render json: @maquinas
+  end
+
+  # GET /maquinas/1
+  def show
+    render json: @maquina
+  end
+
+  # POST /maquinas
+  def create
+    @maquina = Maquina.new(maquina_params)
+
+    if @maquina.save
+      render json: @maquina, status: :created, location: @maquina
+    else
+      render json: @maquina.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /maquinas/1
+  def update
+    if @maquina.update(maquina_params)
+      render json: @maquina
+    else
+      render json: @maquina.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /maquinas/1
+  def destroy
+    @maquina.destroy!
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_maquina
+      @maquina = Maquina.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def maquina_params
+      params.expect(maquina: [ :apelido, :nome_tecnico, :imagem, :descricao ])
+    end
+end
